5d171c07e43526bf37338ce194f34d56bd118405
Rutuja-Ghodekar <92453797+Rutuja-Ghodekar@users.noreply.github.com>
2022-02-26 17:35:11 +0530

    Create Footer.js

---
+++ Task2/src/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <>
+    
+      <div className="bottom">
+        <div class="contain">
+          <div class="arrow">
+            <h1>LGMVIP Task 2</h1>
+            <ul>
+              <li>
+                Mail: <a>ghodekarad@gmail.com</a>
+              </li>
+            </ul>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+    
+    </>
+  );
+}
+export default Footer;
